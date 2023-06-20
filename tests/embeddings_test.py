7fae6ac432edb654f210a934466eac719242b08b
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-06-20 20:42:54 +0300

    Embeddings tests
    Tets for ImageToPatch and PatchEmbeddings classes

--- tests/embeddings_test.py
+++ tests/embeddings_test.py
@@ -3,11 +3,11 @@ import random
 import pytest
 import torch
 
-from src.models.vit.embeddings import PatchEmbeddings
+from src.models.vit.embeddings import ImageToPatches, PatchEmbeddings
 from tests.config import num_tests
 
 
-class TestPatchEmbeddings:
+class TestImageToPatches:
     @pytest.mark.parametrize(
         ("batch_size", "channels", "image_size", "patch_size"),
         list(
@@ -23,10 +23,43 @@ class TestPatchEmbeddings:
         x = torch.randn((batch_size, channels, image_size, image_size))
         B, C, H, W = x.shape
         P = patch_size
-        embeddings = PatchEmbeddings(P)
+        embeddings = ImageToPatches(P)
 
         # When
         out = embeddings(x)
 
         # Then
         assert out.shape == (B, (H // P) * (W // P), C * P * P)
+
+
+class TestPatchEmbeddings:
+    @pytest.mark.parametrize(
+        ("batch_size", "channels", "image_size", "patch_size", "embeddings_size"),
+        list(
+            zip(
+                [random.randint(1, 10) for _ in range(num_tests)],
+                [random.randint(1, 5) for _ in range(num_tests)],
+                *(zip(*[(patch * random.randint(1, 11), patch) for patch in random.sample(range(1, 30), num_tests)])),
+                [random.randint(1, 256) for _ in range(num_tests)],
+            ),
+        ),
+    )
+    def test_return_patches(
+        self,
+        batch_size: int,
+        channels: int,
+        image_size: int,
+        patch_size: int,
+        embeddings_size: int,
+    ) -> None:
+        # Given
+        x = torch.randn((batch_size, channels, image_size, image_size))
+        B, C, H, W = x.shape
+        P = patch_size
+        embeddings = PatchEmbeddings(P, C, embeddings_size)
+
+        # When
+        out = embeddings(x)
+
+        # Then
+        assert out.shape == (B, (H // P) * (W // P), embeddings_size)
